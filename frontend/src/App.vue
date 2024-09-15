38a550733e4d2b709375f0d5ef782ed2e3b8fa22
Yash Agrawal <yash4agr@gmail.com>
2024-09-15 17:59:33 +0530

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+
+  <router-view />
+</template>
+
+
+<script setup>
+import { RouterView, RouterLink} from 'vue-router'
+</script>
---
+++ frontend/src/plugins/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    {path : '', name: 'home', component: () => import('@/views/Home.vue')}
+]
+
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(),
+})
+
+
+export default router
